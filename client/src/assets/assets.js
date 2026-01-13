057fc8e326076489773921a6865bf50e49b07478
swasthikg73 <swasthikg32@gmail.com>
2026-01-13 16:10:05 +0530

    assets added

--- client/src/assets/assets.js
+++ client/src/assets/assets.js
@@ -1,18 +1,13 @@
 import arrow from "./arrow.png";
-import banner from "./banner.png";
 import bed from "./bed.png";
 import bg from "./bg.png";
 import bath from "./bath.png";
-import car from "./car.png";
-import map from "./map.png";
 import logo from "./logo.png";
-import avatar from "./avatar.png";
 import bus from "./bus.png";
 import chat from "./chat.png";
 import favicon from "./favicon.png";
 import search from "./search.png";
 import fee from "./fee.png";
-import logo from "./logo.png";
 import menu from "./menu.png";
 import pet from "./pet.png";
 import pin from "./pin.png";
@@ -24,20 +19,15 @@ import utility from "./utility.png";
 
 export const assets = {
   arrow,
-  banner,
   bed,
   bg,
   bath,
-  car,
-  map,
   logo,
-  avatar,
   bus,
   chat,
   favicon,
   search,
   fee,
-  logo,
   menu,
   pet,
   pin,
