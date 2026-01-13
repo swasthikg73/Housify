db8dcde6083f463af325c9d92e81ae134b4545da
swasthikg73 <swasthikg32@gmail.com>
2026-01-13 16:07:57 +0530

    Navbar added

---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,42 @@
+import "./Navbar.scss";
+import { assets } from "../../assets/assets";
+import { useState } from "react";
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <nav>
+      <div className="left">
+        <a href="" className="logo">
+          <img src={assets.logo} alt="" />
+          <span>Housify</span>
+        </a>
+
+        <a href="/">Home</a>
+        <a href="/">About</a>
+        <a href="/">Contact</a>
+        <a href="/">Agents</a>
+      </div>
+      <div className="right">
+        <a href="/">Sign In</a>
+        <a className="register" href="">
+          Sign Up
+        </a>
+        <div className="menuIcon">
+          <img src={assets.menu} alt="" onClick={() => setOpen(!open)} />
+        </div>
+
+        <div className={open ? "menu active" : "menu"}>
+          <a href="/">Home</a>
+          <a href="/">About</a>
+          <a href="/">Contact</a>
+          <a href="/">Agents</a>
+          <a href="/">Sign In</a>
+          <a href="/">Sign Up</a>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
