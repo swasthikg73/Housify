6302f983f0985d1dd3efff07cd946acee931e1a2
swasthikg73 <swasthikg32@gmail.com>
2026-01-13 16:12:02 +0530

    responsive layout added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,20 @@
-import "./App.css";
+import "./App.scss";
+import Navbar from "./components/Navbar/Navbar.jsx";
+import Home from "./pages/Home/Home.jsx";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="layout">
+        <div className="navbar">
+          <Navbar />
+        </div>
+        <div className="content">
+          <Home />
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
