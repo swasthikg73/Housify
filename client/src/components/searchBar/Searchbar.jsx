249bfa1531520f12ea3ce7ee5414c35af25b24d7
swasthikg73 <swasthikg32@gmail.com>
2026-01-16 14:32:28 +0530

    Search bar added

--- client/src/components/searchBar/Searchbar.jsx
+++ client/src/components/searchBar/Searchbar.jsx
@@ -2,55 +2,54 @@ import "./Searchbar.scss";
 import { assets } from "../../assets/assets.js";
 import { useState } from "react";
 
-const types = ["Buy", "Rent"];
+const types = ["buy", "rent"];
 
-const Searchbar = () => {
-  const [data, setData] = useState({
-    type: "Buy",
+function SearchBar() {
+  const [query, setQuery] = useState({
+    type: "buy",
     location: "",
     minPrice: 0,
     maxPrice: 0,
   });
 
-  const switchType = (value) => {
-    setData((prev) => ({ ...prev, type: value }));
-    6;
+  const switchType = (val) => {
+    setQuery((prev) => ({ ...prev, type: val }));
   };
 
   return (
-    <div className="search-container">
+    <div className="searchBar">
       <div className="type">
         {types.map((type) => (
           <button
-            onClick={() => switchType(type)}
             key={type}
-            className={data.type === type ? "active" : ""}>
+            onClick={() => switchType(type)}
+            className={query.type === type ? "active" : ""}>
             {type}
           </button>
         ))}
       </div>
-      <form action="">
+      <form>
         <input type="text" name="location" placeholder="City Location" />
         <input
           type="number"
           name="minPrice"
           min={0}
-          max={1000000}
+          max={10000000}
           placeholder="Min Price"
         />
         <input
           type="number"
           name="maxPrice"
           min={0}
-          max={1000000}
+          max={10000000}
           placeholder="Max Price"
         />
-        <button type="submit">
+        <button>
           <img src={assets.search} alt="" />
         </button>
       </form>
     </div>
   );
-};
+}
 
-export default Searchbar;
+export default SearchBar;
