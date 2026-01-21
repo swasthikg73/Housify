794fe628645012a88a046957a06c25c42c929bca
swasthikg73 <swasthikg32@gmail.com>
2026-01-21 12:00:34 +0530

    Map component added

---
+++ client/src/components/Map/Map.jsx
@@ -0,0 +1,24 @@
+import "./Map.scss";
+import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+import Pin from "../Pin/Pin";
+import { ListData } from "../../assets/assets.js";
+
+const Map = () => {
+  return (
+    <div className="map">
+      <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={true}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+
+        {ListData.map((item) => (
+          <Pin key={item.id} item={item} />
+        ))}
+      </MapContainer>
+    </div>
+  );
+};
+
+export default Map;
