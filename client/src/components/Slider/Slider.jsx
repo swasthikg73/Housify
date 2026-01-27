bd38254ad532abc20abf2c218773454d4e54b4fb
swasthikg73 <swasthikg32@gmail.com>
2026-01-27 14:25:09 +0530

    Slider component added

--- client/src/components/Slider/Slider.jsx
+++ client/src/components/Slider/Slider.jsx
@@ -1,8 +1,21 @@
+import { assets } from "../../assets/assets";
 import "./Slider.scss";
 
 const Slider = ({ images }) => {
   return (
     <div className="slider">
+      <div className="fullSlider">
+        <div className="arrow">
+          <img src={assets.arrow} alt="" />
+        </div>
+        <div className="imgContainer">
+          <img src={images[0]} alt="" />
+        </div>
+        <div className="arrow">
+          <img src={assets.arrow} className="right" alt="" />
+        </div>
+        <div className="close">X</div>
+      </div>
       <div className="bigImage">
         <img src={images[0]} alt="" />
       </div>
