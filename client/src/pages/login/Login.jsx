a96b383b5050a1a7c2bfdc2da078f5d598e8bbff
swasthikg73 <swasthikg32@gmail.com>
2026-02-03 10:25:14 +0530

    Login flow added

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,68 +1,53 @@
 import "./Login.scss";
 import { assets } from "../../assets/assets";
-import { useState } from "react";
+import apiRequest from "../../lib/apiRequest.js";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 const Login = () => {
-  const [register, setRegister] = useState(true);
+  const navigate = useNavigate();
+  const onSubmitHandler = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
 
-  const form = {
-    name: "",
-    email: "",
-    password: "",
-  };
+    const email = formData.get("email");
+    const password = formData.get("password");
 
-  const onSubmit = () => {
-    e.target.preventDefault();
-    console.log("Clicked");
+    try {
+      const res = await apiRequest.post("/auth/login", {
+        email,
+        password,
+      });
+      console.log(res.data);
 
-    console.log(form);
+      if (res.data.success) {
+        toast.success(res.data.message);
+        navigate("/");
+      } else {
+        toast.error(res.data.message);
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error(error.response.data.message);
+    }
   };
 
   return (
     <div className="form-wrapper">
       <div className="form">
-        <form>
-          {register ? <h1>Create an Account</h1> : <h1>Welcome back,</h1>}
-          {register && (
-            <input
-              name="username"
-              type="text"
-              value={form.name}
-              placeholder="Username"
-            />
-          )}
-          <input
-            name="email"
-            type="text"
-            value={form.email}
-            placeholder="Email"
-          />
-          <input
-            name="password"
-            type="password"
-            value={form.password}
-            placeholder="Password"
-          />
-          <button onClick={(e) => onSubmit(e)} type="submit">
-            {register ? "Register" : "Login"}
-          </button>
+        <form onSubmit={onSubmitHandler}>
+          <h1>Welcome back,</h1>
+          <input name="email" type="text" placeholder="Email" />
+          <input name="password" type="password" placeholder="Password" />
+          <button type="submit">Login</button>
         </form>
 
-        {register ? (
-          <p>
-            Already have an
-            <span className="link" onClick={() => setRegister(!register)}>
-              Account?
-            </span>
-          </p>
-        ) : (
-          <p>
-            Don't you have any
-            <span className="link" onClick={() => setRegister(!register)}>
-              Account?
-            </span>
-          </p>
-        )}
+        <p>
+          Don't you have any
+          <Link to="/register" className="link">
+            Account?
+          </Link>
+        </p>
       </div>
       <div className="imageContainer">
         <img src={assets.bg} alt="" />
