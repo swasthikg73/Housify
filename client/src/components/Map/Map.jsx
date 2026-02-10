af0ac248312f55feb9b2c8b28eaa27c03233845a
swasthikg73 <swasthikg32@gmail.com>
2026-02-10 10:52:47 +0530

    Map Component updated

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -1,21 +1,34 @@
 import "./Map.scss";
-import { MapContainer, Marker, TileLayer, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Pin from "../Pin/Pin";
 import { ListData } from "../../assets/assets.js";
 
 const Map = ({ items }) => {
+  // console.log("----------------------------------------------------");
+  // console.log("items :", items);
+
+  // console.log(items.length);
+
+  // console.log("----------------------------------------------------");
+
   return (
     <div className="map">
-      <MapContainer center={[51.505, -0.09]} zoom={7} scrollWheelZoom={true}>
+      <MapContainer
+        center={
+          items ? [items[0]?.latitude, items[0]?.longitude] : [12.9629, 77.5775]
+        }
+        zoom={8}
+        scrollWheelZoom={true}>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
 
-        {items && <Pin item={items} />}
+        {items?.length === 1 && <Pin item={items[0]} />}
 
-        {!items && ListData.map((item) => <Pin key={item.id} item={item} />)}
+        {items?.length > 1 &&
+          items?.map((item) => <Pin key={item?.id} item={item} />)}
       </MapContainer>
     </div>
   );
