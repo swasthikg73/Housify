b774ba38b5b75192bf52417d346954b831e8fe55
swasthikg73 <swasthikg32@gmail.com>
2026-02-16 12:04:18 +0530

    fixes : Map Pin issue on Production

--- client/src/components/Map/Map.jsx
+++ client/src/components/Map/Map.jsx
@@ -2,16 +2,21 @@ import "./Map.scss";
 import { MapContainer, TileLayer } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import Pin from "../Pin/Pin";
-import { ListData } from "../../assets/assets.js";
+import L from "leaflet";
+
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+delete L.Icon.Default.prototype._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
 
 const Map = ({ items }) => {
-  // console.log("----------------------------------------------------");
-  // console.log("items :", items);
-
-  // console.log(items.length);
-
-  // console.log("----------------------------------------------------");
-
   return (
     <div className="map">
       <MapContainer
--- client/src/lib/apiRequest.js
+++ client/src/lib/apiRequest.js
@@ -1,8 +1,26 @@
 import axios from "axios";
 
+let ProductionURL = "https://housify-sever.onrender.com/api";
+let DevelopmentURL = "http://localhost:5000/api";
 const apiRequest = axios.create({
-  baseURL: "https://housify-sever.onrender.com/api",
+  baseURL: ProductionURL,
   withCredentials: true,
 });
+
+apiRequest.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    const message =
+      error.response?.data?.message || error.message || "Something went wrong";
+
+    // Dispatch global event
+    window.dispatchEvent(new CustomEvent("api-error", { detail: message }));
+
+    // if (error.response?.status === 401) {
+    //   window.location.href = "/login";
+    // }
+    return Promise.reject(error);
+  }
+);
 
 export default apiRequest;
